51a2509e45770b0da0cb5d6e81292e1c2572fc53
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-01-26 03:39:39 -0300

    Aplicativo funcionando (ainda muitos bugs)

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
-from tkinter import *
 from tkinter import filedialog
+import tkinter as tk
+import PyPDF2 as pdf
+import pyttsx3 as pts
 
+# Abre janela para selecionar pdf
 def select_pdf_file():
-  root = Tk()
-  root.withdraw()
-  file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
-  root.destroy()
-  return file_path
+  audio_text["text"] = "Rodando áudio..."
 
-print(select_pdf_file())
+  file_search = tk.Tk()
+  file_search.withdraw()
+  file_path = filedialog.askopenfilename(filetypes=[("PDF files", "*.pdf")])
+  file_search.destroy()
+  return read_pdf(file_path)
+
+def read_pdf(pdf_path):  
+  # Abre o E-book e faz a letura das informações
+  path = open(pdf_path, "rb")
+  pdf_reader = pdf.PdfReader(path)
+
+  speak = pts.init()
+
+  # Percorre as páginas do E-book, extrai o texto e faz a leitura
+  for page in pdf_reader.pages:
+    text = page.extract_text()
+    speak.say(text)
+    speak.runAndWait()
+  speak.stop()
+
+  audio_text["text"] = "Áudio finalizado"
+
+# Cria a janela
+window = tk.Tk()
+window.resizable(False, False)
+window.title("Conversor de Audiobook")
+
+text_guindance = tk.Label(window, text="Clique no botão abaixo para selecionar um E-book:")
+text_guindance.grid(column=0, row=0, padx=10, pady=[10, 5])
+
+test = 0
+
+button = tk.Button(window, text="Buscar PDF", command=select_pdf_file)
+button.grid(column=0, row=1, padx=10, pady=[5, 0])
+
+audio_text = tk.Label(window, text="")
+audio_text.grid(column=0, row=2, padx=10, pady=10)
+
+window.mainloop()
